392c0deb2fc9a3031fe83d4e234a4b7f1e9d9f62
natasa <natasa@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2008-03-11 14:19:47 +0000

    dodana metoda za getText iz dokumenta
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@1682 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/util/Document.java
+++ src/com/openitech/util/Document.java
@@ -17,17 +17,28 @@ import javax.swing.text.BadLocationException;
  * @author uros
  */
 public class Document {
-  
-  /** Creates a new instance of Document */
-  private Document() {
-  }
-  
-  public static void setText(javax.swing.text.Document doc, String t) throws BadLocationException {
-    if (doc instanceof AbstractDocument) {
-      ((AbstractDocument)doc).replace(0, doc.getLength(), t,null);
-    } else {
-      doc.remove(0, doc.getLength());
-      doc.insertString(0, t, null);
+    
+    /** Creates a new instance of Document */
+    private Document() {
     }
-  }
+    
+    public static void setText(javax.swing.text.Document doc, String t) throws BadLocationException {
+        if (doc instanceof AbstractDocument) {
+            ((AbstractDocument)doc).replace(0, doc.getLength(), t,null);
+        } else {
+            doc.remove(0, doc.getLength());
+            doc.insertString(0, t, null);
+        }
+    }
+    
+    public static String getText(javax.swing.text.Document doc) throws BadLocationException {
+        String result;
+        try {
+            result = doc.getText(0, doc.getLength());
+        } catch (BadLocationException err) {
+            result = "";
+        }
+        
+        return result;
+    }
 }
