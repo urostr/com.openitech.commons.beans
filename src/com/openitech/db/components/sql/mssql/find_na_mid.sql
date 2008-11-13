a6af245a6e738263888c253be2928a314a721260
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2008-11-13 10:40:08 +0000

    sql
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@2405 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/db/components/sql/mssql/find_na_mid.sql
+++ src/com/openitech/db/components/sql/mssql/find_na_mid.sql
@@ -1,8 +1,19 @@
 SELECT DISTINCT   
- NA_s.na_mid
-FROM         NA_s LEFT OUTER JOIN
-                      UL_PT_s ON NA_s.na_mid = UL_PT_s.na_mid LEFT OUTER JOIN
-                      PT_s ON UL_PT_s.pt_mid = PT_s.pt_mid   
+    NA_s.na_mid
+FROM         
+    NA_s 
+LEFT OUTER JOIN
+    UL_PT_s 
+    ON 
+    (
+            NA_s.na_mid = UL_PT_s.na_mid 
+    )        
+LEFT OUTER JOIN
+    PT_s 
+    ON 
+    (
+            UL_PT_s.pt_mid = PT_s.pt_mid  
+    )       
 WHERE 
      NA_s.na_mid is not null AND
     PT_s.pt_id = ? AND
--- src/com/openitech/db/components/sql/mssql/find_ul_mid.sql
+++ src/com/openitech/db/components/sql/mssql/find_ul_mid.sql
@@ -1,8 +1,25 @@
-SELECT DISTINCT  UL_s.ul_mid
-FROM         UL_s LEFT OUTER JOIN
-                      UL_PT_s ON UL_s.ul_mid = UL_PT_s.ul_mid LEFT OUTER JOIN
-                      PT_s ON UL_PT_s.pt_mid = PT_s.pt_mid LEFT OUTER JOIN
-                      NA_s ON UL_s.na_mid = NA_s.na_mid        
+SELECT DISTINCT 
+    UL_s.ul_mid
+FROM         
+    UL_s 
+LEFT OUTER JOIN
+    UL_PT_s 
+    ON 
+    (
+              UL_s.ul_mid = UL_PT_s.ul_mid 
+    )             
+LEFT OUTER JOIN
+    PT_s 
+    ON 
+    (
+              UL_PT_s.pt_mid = PT_s.pt_mid 
+    )          
+LEFT OUTER JOIN
+    NA_s 
+    ON 
+    (
+              UL_s.na_mid = NA_s.na_mid    
+    )
 WHERE 
     UL_s.ul_mid is not null AND
     UL_s.ul_ime = ? AND
