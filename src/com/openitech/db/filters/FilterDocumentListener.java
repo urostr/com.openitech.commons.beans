12a65d24f84fe31c1e77d9f6da2030ea2813ace5
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2009-01-19 15:04:32 +0000

    popravljeno, da dela z datumom
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@2767 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/db/filters/FilterDocumentListener.java
+++ src/com/openitech/db/filters/FilterDocumentListener.java
@@ -1,50 +1,67 @@
 package com.openitech.db.filters;
 
 import com.openitech.db.filters.Scheduler.SeekValueUpdateRunnable;
+import com.openitech.formats.FormatFactory;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 
-
 public class FilterDocumentListener extends Scheduler implements DocumentListener {
-  protected DataSourceFilters filter;
-  protected DataSourceFilters.AbstractSeekType<? extends Object> seek_type;
-  
-  public FilterDocumentListener(DataSourceFilters filter, DataSourceFilters.AbstractSeekType<? extends Object> seek_type) {
-    this(filter, seek_type, Scheduler.DELAY);
-  }
-  
-  public FilterDocumentListener(DataSourceFilters filter, DataSourceFilters.AbstractSeekType<? extends Object> seek_type, long delay) {
-    this.filter = filter;
-    this.seek_type = seek_type;
-    this.delay = delay;
-  }
-  
-  public String getText(DocumentEvent e) {
-    return getText(e.getDocument());
-  }
 
-  public String getText(Document doc) {
-    String txt;
-    try {
-      txt = doc.getText(0, doc.getLength());
-    } catch (BadLocationException err) {
-      txt = null;
+    protected DataSourceFilters filter;
+    protected DataSourceFilters.AbstractSeekType<? extends Object> seek_type;
+
+    public FilterDocumentListener(DataSourceFilters filter, DataSourceFilters.AbstractSeekType<? extends Object> seek_type) {
+        this(filter, seek_type, Scheduler.DELAY);
     }
-    return txt;  }
-  
-  public void changedUpdate(DocumentEvent e) {
-    setSeekValue(e);
-  }
-  public void insertUpdate(DocumentEvent e) {
-    setSeekValue(e);
-  }
-  public void removeUpdate(DocumentEvent e) {
-    setSeekValue(e);
-  }
-  
-  protected void setSeekValue(final DocumentEvent e) {
-    schedule(new SeekValueUpdateRunnable<Object>(filter, seek_type, getText(e)));
-  }
+
+    public FilterDocumentListener(DataSourceFilters filter, DataSourceFilters.AbstractSeekType<? extends Object> seek_type, long delay) {
+        this.filter = filter;
+        this.seek_type = seek_type;
+        this.delay = delay;
+    }
+
+    public String getText(DocumentEvent e) {
+        return getText(e.getDocument());
+    }
+
+    public String getText(Document doc) {
+        String txt;
+        try {
+            txt = doc.getText(0, doc.getLength());
+        } catch (BadLocationException err) {
+            txt = null;
+        }
+        return txt;
+    }
+
+    public void changedUpdate(DocumentEvent e) {
+        setSeekValue(e);
+    }
+
+    public void insertUpdate(DocumentEvent e) {
+        setSeekValue(e);
+    }
+
+    public void removeUpdate(DocumentEvent e) {
+        setSeekValue(e);
+    }
+
+    protected void setSeekValue(final DocumentEvent e) {
+        if (seek_type instanceof DataSourceFilters.DateSeekType) {
+            try {
+                java.util.Date value = FormatFactory.DATE_FORMAT.parse(getText(e));
+                schedule(new SeekValueUpdateRunnable<Object>(filter, seek_type, value));
+            } catch (ParseException ex) {
+                Logger.getLogger(FilterDocumentListener.class.getName()).log(Level.WARNING, ex.getMessage());
+                schedule(new SeekValueUpdateRunnable<Object>(filter, seek_type, null));
+            }
+        } else {
+            schedule(new SeekValueUpdateRunnable<Object>(filter, seek_type, getText(e)));
+        }
+    }
 }
