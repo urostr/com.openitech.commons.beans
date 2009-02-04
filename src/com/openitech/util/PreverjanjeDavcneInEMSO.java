ebf73c5d0f4eb3477e1046aaae51c43fd6b1d004
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2009-02-04 09:14:50 +0000

    preverjanje emša
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@2867 7140e42c-79bd-4eef-9ff7-51c9bd73192f

---
+++ src/com/openitech/util/PreverjanjeDavcneInEMSO.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openitech.util;
+
+/**
+ *
+ * @author DomenBasic
+ */
+public class PreverjanjeDavcneInEMSO {
+
+  public static Boolean preveriDavcno(int davcna) {
+    int[] tabelaDavcna = new int[8];
+
+    for (int i = 7; i >= 0; i--) {
+      tabelaDavcna[i] = davcna % 10;
+      davcna = davcna / 10;
+    }
+
+
+    int kontrolnaStevilka;
+    int sestevekZmnozkov = 0;
+
+    sestevekZmnozkov = 8 * tabelaDavcna[0] + 7 * tabelaDavcna[1] + 6 * tabelaDavcna[2] + 5 * tabelaDavcna[3] + 4 * tabelaDavcna[4] + 3 * tabelaDavcna[5] + 2 * tabelaDavcna[6];
+
+   int  ostanekPriDeljenju = sestevekZmnozkov % 11;
+
+    kontrolnaStevilka = 11 - ostanekPriDeljenju;
+    if (kontrolnaStevilka == 10) {
+      kontrolnaStevilka = 0;
+    } else if (kontrolnaStevilka == 11) {
+      return false;
+    }
+    if (kontrolnaStevilka == tabelaDavcna[7]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static Boolean preveriEMSO(long emso) {
+    long[] tabelaEMSO = new long[13];
+    if(emso > 9999999999999L){
+      return false;
+    }
+    for (int i = 12; i >= 0; i--) {
+      tabelaEMSO[i] = emso % 10;
+      emso = emso / 10;
+    }
+
+    long kontrolnaStevilka;
+    long sestevekZmnozkov = 0L;
+
+
+    for (int i = 0; i < 12; i++) {
+      sestevekZmnozkov = 7 * tabelaEMSO[0] + 6 * tabelaEMSO[1] + 5 * tabelaEMSO[2] + 4 * tabelaEMSO[3] + 3 * tabelaEMSO[4] + 2 * tabelaEMSO[5] + 7 * tabelaEMSO[6] + 6 * tabelaEMSO[7] + 5 * tabelaEMSO[8] + 4 * tabelaEMSO[9] + 3 * tabelaEMSO[10] + 2 * tabelaEMSO[11];
+    }
+
+    long ostanekPriDeljenju = sestevekZmnozkov % 11;
+    kontrolnaStevilka = 11 - ostanekPriDeljenju;
+    if(kontrolnaStevilka == 11){
+      kontrolnaStevilka=0;
+    }else if(kontrolnaStevilka == 10){
+      return false;
+    }
+    
+    if (kontrolnaStevilka == tabelaEMSO[12]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
