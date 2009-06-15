ab94937cf6fe62df9af667715c15af514cb8bf1d
uros <uros@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2009-06-15 12:02:21 +0000

    popravljen nacin delitve pri tujih telefonskih stevilkah
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@3456 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/util/TelefonskeStevilke.java
+++ src/com/openitech/util/TelefonskeStevilke.java
@@ -13,22 +13,26 @@ import java.util.regex.Pattern;
  */
 public class TelefonskeStevilke {
 
-  private static final Pattern PT_TELEFONSKA = Pattern.compile("^((00|\\+)((1\\s?(340|670|787|868))|7|20|27|3[0-4]|36|39|4[0-1]|4[3-9]|5[1-8]|6[0-6]|8[1-2]|84|86|9[0-5]|98|21[2-3]|216|218|2[2-4][0-9]|25[0-8]|26[0-9]|284|29[0-1]|29[7-8]|35[0-9]|37[0-6]|378|38[0-1]|38[5-7]|389|42[0-1]|423|473|50[0-9]|59[0-9]|6[7-8][0-9]|69[0-2]|767|809|850|852|853|855|869|876|880|886|96[0-8]|97[1-7]|99[3-6]))??((0)?([3-5][0-1]|64|[7-9]0|59[0-2]|597|599|817|[1-8]))(\\D*)(\\d+)");
+  private static final Pattern PT_TELEFONSKA = Pattern.compile("^((00|\\+)((1\\s?(340|670|787|868))|7|20|27|3[0-4]|36|39|4[0-1]|4[3-9]|5[1-8]|6[0-6]|8[1-2]|84|86|9[0-5]|98|21[2-3]|216|218|2[2-4][0-9]|25[0-8]|26[0-9]|284|29[0-1]|29[7-8]|35[0-9]|37[0-6]|378|38[0-1]|38[5-7]|389|42[0-1]|423|473|50[0-9]|59[0-9]|6[7-8][0-9]|69[0-2]|767|809|850|852|853|855|869|876|880|886|96[0-8]|97[1-7]|99[3-6]))??(((0)?([3-5][0-1]|64|[7-9]0|59[0-2]|597|599|817|[1-8]))(\\D*)(\\d+))");
   private static final String TL_SLOVENIJA = "386";
 
   public static Telefon getTelefon(String telefonskaStevilka) {
     StringBuffer digits = new StringBuffer(telefonskaStevilka.length());
     for (int pos = 0; pos < telefonskaStevilka.length(); pos++) {
-      if (Character.isDigit(telefonskaStevilka.charAt(pos))||(telefonskaStevilka.charAt(pos)=='+')) {
+      if (Character.isDigit(telefonskaStevilka.charAt(pos)) || (telefonskaStevilka.charAt(pos) == '+')) {
         digits.append(telefonskaStevilka.charAt(pos));
       }
     }
     Matcher m_telefonska = PT_TELEFONSKA.matcher(digits);
     if (m_telefonska.matches()) {
       Telefon result = new Telefon();
-      result.setDrzava(m_telefonska.group(3)==null?TL_SLOVENIJA:m_telefonska.group(3));
-      result.setOmrezna(m_telefonska.group(8));
-      result.setTelefonska(m_telefonska.group(10));
+      result.setDrzava(m_telefonska.group(3) == null ? TL_SLOVENIJA : m_telefonska.group(3));
+      if (result.getDrzava().equals(TL_SLOVENIJA)) {
+        result.setOmrezna(m_telefonska.group(9));
+        result.setTelefonska(m_telefonska.group(11));
+      } else {
+        result.setTelefonska(m_telefonska.group(6));
+      }
 
       return result;
     } else {
@@ -59,7 +63,7 @@ public class TelefonskeStevilke {
     }
 
     public Telefon(String omrezna, String telefonska) {
-      this( TL_SLOVENIJA, omrezna,telefonska);
+      this(TL_SLOVENIJA, omrezna, telefonska);
     }
 
     public Telefon(String drzava, String omrezna, String telefonska) {
