99ac66bd06c5197c6a23a198ce6b732a3008cfd7
uros <uros@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2009-07-14 07:04:25 +0000

    ne rabimo vseh testov
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@3530 7140e42c-79bd-4eef-9ff7-51c9bd73192f

---
+++ test/com/openitech/util/TelefonskeStevilkeTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.openitech.util;
+
+import com.openitech.util.TelefonskeStevilke.Telefon;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author uros
+ */
+public class TelefonskeStevilkeTest extends TestCase {
+    
+    public TelefonskeStevilkeTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+  /**
+   * Test of getTelefon method, of class TelefonskeStevilke.
+   */
+  public void testGetTelefon() {
+    System.out.println("getTelefon");
+    Telefon expResult = new Telefon("386", "41", "765030");
+    Telefon result = TelefonskeStevilke.getTelefon("041/765-030");
+    assertEquals(expResult, result);
+
+    result = TelefonskeStevilke.getTelefon("+38641765030");
+    assertEquals(expResult, result);
+  }
+
+}
