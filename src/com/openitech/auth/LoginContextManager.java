a91e19f0760db8ac9e1858e61be75fa991ea8026
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2009-10-23 08:35:36 +0000

    dodano logiranje za dodatno (remote) povezavo
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@4007 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/auth/LoginContextManager.java
+++ src/com/openitech/auth/LoginContextManager.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package com.openitech.auth;
 
 import com.openitech.util.Equals;
@@ -34,34 +33,31 @@ import org.jdesktop.swingx.JXLoginPane;
 import org.jdesktop.swingx.auth.LoginService;
 import com.openitech.auth.jaas.LocalGroupPrincipal;
 import com.openitech.auth.jaas.callback.LocalCommitCallback;
-
+import java.awt.Component;
 
 /**
  *
  * @author uros
  */
 public class LoginContextManager extends PropertyChanges {
-  
+
   private static final Logger LOG = Logger.getLogger(LoginContextManager.class.getCanonicalName());
   private static LoginContextManager instance = null;
-  
   private Map<Class<? extends Callback>, Set<CallbackHandler>> handlers = Collections.synchronizedMap(new HashMap<Class<? extends Callback>, Set<CallbackHandler>>());
   private LoginContext context;
-  
   private String server = "other";
   private String name = null;
   private char[] password = null;
   private Set<String> userRoles = new HashSet<String>();
-  
   private JAASCallbackHandler jaasCallbackHandler = new JAASCallbackHandler();
-  
+
   /** Creates a new instance of LoginContextManager */
-  private LoginContextManager() throws LoginException {
+  public LoginContextManager() throws LoginException {
     javax.security.auth.login.Configuration.setConfiguration(new com.openitech.auth.jaas.Configuration());
   }
-  
+
   public static LoginContextManager getInstance() {
-    if (instance==null) {
+    if (instance == null) {
       try {
         instance = new LoginContextManager();
       } catch (LoginException ex) {
@@ -69,80 +65,114 @@ public class LoginContextManager extends PropertyChanges {
         instance = null;
       }
     }
-    
+
     return instance;
   }
-  
+
   private void contextLogin() throws LoginException {
-    if (context==null) {
+    if (context == null) {
       context = new LoginContext(getServer(), jaasCallbackHandler);
     }
     firePropertyChange("connecting", false, true);
     context.login();
   }
-  
-  private void contextLogout()  {
-    if (context!=null) {
+
+  private void contextLogout() {
+    if (context != null) {
       try {
         context.logout();
       } catch (LoginException ex) {
         LOG.log(Level.WARNING, "Couldn't close the security context", ex);
       }
-      context=null;
+      context = null;
       userRoles.clear();
     }
   }
-  
+
   public boolean isLoggedOn() {
-    return ((getSubject()!=null)&&(getSubject().getPrincipals(com.openitech.auth.jaas.LocalPrincipal.class).size()>0));
+    return ((getSubject() != null) && (getSubject().getPrincipals(com.openitech.auth.jaas.LocalPrincipal.class).size() > 0));
   }
-  
+
   public Subject getSubject() {
-    return context!=null?context.getSubject():null;
+    return context != null ? context.getSubject() : null;
   }
-  
+
   public void logon() throws LoginException {
     logon(getOwner());
   }
-  
+
   public void logon(Object source) throws LoginException {
     try {
       com.openitech.db.DbConnection dbConnection = com.openitech.db.ConnectionManager.getInstance();
 
-      name = name==null?dbConnection.getProperty(com.openitech.db.DbConnection.DB_USER, null):name;
+      name = name == null ? dbConnection.getProperty(com.openitech.db.DbConnection.DB_USER, null) : name;
       String pwd = dbConnection.getProperty(com.openitech.db.DbConnection.DB_PASS, null);
-      if ((password==null)&&(pwd!=null)) {
-        password=pwd.toCharArray();
+      if ((password == null) && (pwd != null)) {
+        password = pwd.toCharArray();
       }
     } catch (Exception ex) {
       //ignore it
     }
-    if (name==null||
-        password==null) {
-      java.awt.Component owner = (source instanceof java.awt.Component)?((java.awt.Component) source):null;
-      JXLoginPane.showLoginDialog(owner, new LocalLoginService());
+    if (name == null ||
+            password == null) {
+      java.awt.Component parrent = (source instanceof java.awt.Component) ? ((java.awt.Component) source) : null;
+      JXLoginPane.showLoginDialog(parrent, new LocalLoginService());
     } else {
       contextLogin();
     }
     final boolean newValue = isLoggedOn();
     EventQueue.invokeLater(new Runnable() {
+
+      @Override
       public void run() {
         firePropertyChange("logon", !newValue, newValue);
       }
     });
   }
-  
+
+  public void logon(Component source, boolean autoLogin) throws LoginException {
+    if (autoLogin) {
+      try {
+        com.openitech.db.DbConnection dbConnection = com.openitech.db.ConnectionManager.getInstance();
+
+        name = name == null ? dbConnection.getProperty(com.openitech.db.DbConnection.DB_USER, null) : name;
+        String pwd = dbConnection.getProperty(com.openitech.db.DbConnection.DB_PASS, null);
+        if ((password == null) && (pwd != null)) {
+          password = pwd.toCharArray();
+        }
+      } catch (Exception ex) {
+        //ignore it
+      }
+    }
+    if (name == null ||
+            password == null) {
+      JXLoginPane.showLoginDialog(source, new LocalLoginService());
+    } else {
+      contextLogin();
+    }
+    final boolean newValue = isLoggedOn();
+    EventQueue.invokeLater(new Runnable() {
+
+      @Override
+      public void run() {
+        firePropertyChange("logon", !newValue, newValue);
+      }
+    });
+  }
+
   public void logout() {
     final boolean oldValue = isLoggedOn();
     contextLogout();
     final boolean newValue = isLoggedOn();
     EventQueue.invokeLater(new Runnable() {
+
+      @Override
       public void run() {
         firePropertyChange("logout", oldValue, newValue);
       }
     });
   }
-  
+
   public void clearState() {
     final boolean oldValue = isLoggedOn();
     contextLogout();
@@ -151,18 +181,20 @@ public class LoginContextManager extends PropertyChanges {
     userRoles.clear();
     final boolean newValue = isLoggedOn();
     EventQueue.invokeLater(new Runnable() {
+
+      @Override
       public void run() {
         firePropertyChange("logout", oldValue, newValue);
       }
     });
   }
-  
+
   public void registerHandler(Class<? extends Callback> type, CallbackHandler handler) {
-    Set<CallbackHandler> set = handlers.containsKey(type)?handlers.get(type):Collections.synchronizedSet(new HashSet<CallbackHandler>());
+    Set<CallbackHandler> set = handlers.containsKey(type) ? handlers.get(type) : Collections.synchronizedSet(new HashSet<CallbackHandler>());
     set.add(handler);
     handlers.put(type, set);
   }
-  
+
   public void unregisterHandler(Class<? extends Callback> type, CallbackHandler handler) {
     if (handlers.containsKey(type)) {
       Set<CallbackHandler> set = handlers.get(type);
@@ -170,8 +202,9 @@ public class LoginContextManager extends PropertyChanges {
       handlers.put(type, set);
     }
   }
-    
+
   private class LocalLoginService extends LoginService {
+
     /**
      * This method is intended to be implemented by clients
      * wishing to authenticate a user with a given password.
@@ -185,6 +218,7 @@ public class LoginContextManager extends PropertyChanges {
      * @return <code>true</code> on authentication success
      * @throws Exception
      */
+    @Override
     public boolean authenticate(String aname, char[] apassword, String server) throws Exception {
       try {
         name = aname;
@@ -196,46 +230,48 @@ public class LoginContextManager extends PropertyChanges {
         return false;
       }
     }
-    
+
     /**
      * Called immediately after a successful authentication. This method should return an array
      * of user roles or null if role based permissions are not used.
      *
      * @return per default <code>null</code>
      */
+    @Override
     public String[] getUserRoles() {
-      if (userRoles==null) {
+      if (userRoles == null) {
         return null;
       } else {
         return (String[]) userRoles.toArray();
       }
     }
   }
-  
+
   private class JAASCallbackHandler implements CallbackHandler {
+
+    @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-      for (Callback callback:callbacks) {
+      for (Callback callback : callbacks) {
         if (callback instanceof NameCallback) {
           ((NameCallback) callback).setName(name);
         } else if (callback instanceof PasswordCallback) {
           ((PasswordCallback) callback).setPassword(password);
         } else {
           if (callback instanceof LocalCommitCallback) {
-            for (LocalGroupPrincipal supplementaryGroup:((LocalCommitCallback) callback).getSupplementaryGroups()) {
+            for (LocalGroupPrincipal supplementaryGroup : ((LocalCommitCallback) callback).getSupplementaryGroups()) {
               userRoles.add(supplementaryGroup.getName());
             }
           }
           if (handlers.containsKey(callback.getClass())) {
             Set<CallbackHandler> set = handlers.get(callback.getClass());
-            for (Iterator<CallbackHandler> iterator=set.iterator(); iterator.hasNext(); ) {
-              iterator.next().handle(new Callback[] {callback});
+            for (Iterator<CallbackHandler> iterator = set.iterator(); iterator.hasNext();) {
+              iterator.next().handle(new Callback[]{callback});
             }
           }
         }
       }
     }
   }
-
   /**
    * Holds value of property owner.
    */
@@ -285,8 +321,16 @@ public class LoginContextManager extends PropertyChanges {
   public Set<String> getUserRoles() {
     return this.userRoles;
   }
-  
+
   public String getName() {
     return this.name;
+  }
+
+  public String getUserName() {
+    return this.name;
+  }
+
+  public String getPassword() {
+    return new String(this.password);
   }
 }
