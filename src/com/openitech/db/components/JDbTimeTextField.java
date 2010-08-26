f99c27374671058d3ab6c8089058a2d6d2919fd5
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2010-08-26 08:20:50 +0000

    time auto text
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@5209 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/db/components/JDbTimeTextField.java
+++ src/com/openitech/db/components/JDbTimeTextField.java
@@ -6,23 +6,51 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package com.openitech.db.components;
 
+import com.openitech.Settings;
 import com.openitech.text.FormatFactory;
-
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author uros
  */
 public class JDbTimeTextField extends JDbFormattedTextField {
-  
+
   /** Creates a new instance of JDbDateTextField */
   public JDbTimeTextField() {
     super();
     setFormat(FormatFactory.TIME_FORMAT);
     setColumns(8);
+  }
+
+  @Override
+  public String getText() {
+    String text = super.getText();
+
+    if (text != null) {
+      StringBuilder sb = new StringBuilder(text);
+      if (text.length() == 1) {
+        text = sb.insert(0, "0").append(":00:00").toString();
+      } else if (text.length() == 2) {
+        text = sb.append(":00:00").toString();
+      } else if (text.length() == 3) {
+        if (sb.indexOf(":") < 0) {
+          text = sb.insert(0, "0").insert(2, ":").append(":00").toString();
+        }
+      } else if (text.length() == 4) {
+        if (sb.indexOf(":") < 0) {
+          text = sb.insert(2, ":").append(":00").toString();
+        }
+      } else if (text.length() == 5) {
+        text = sb.append(":00").toString();
+      }
+    }
+    return text;
   }
   
 }
