f3ba8a08e0506f19a34803c3932db06dc12f7796
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2011-01-05 02:07:49 +0000

    popravljeno preverjanje davcne in emso
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@5934 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/util/PreverjanjeDavcneInEMSO.java
+++ src/com/openitech/util/PreverjanjeDavcneInEMSO.java
@@ -11,6 +11,9 @@ package com.openitech.util;
 public class PreverjanjeDavcneInEMSO {
 
   public static Boolean preveriDavcno(int davcna) {
+    if (davcna > 99999999) {//ce ima vec kot 8 stevilk ni veljavna
+      return false;
+    }
     int[] tabelaDavcna = new int[8];
 
     for (int i = 7; i >= 0; i--) {
@@ -24,7 +27,7 @@ public class PreverjanjeDavcneInEMSO {
 
     sestevekZmnozkov = 8 * tabelaDavcna[0] + 7 * tabelaDavcna[1] + 6 * tabelaDavcna[2] + 5 * tabelaDavcna[3] + 4 * tabelaDavcna[4] + 3 * tabelaDavcna[5] + 2 * tabelaDavcna[6];
 
-   int  ostanekPriDeljenju = sestevekZmnozkov % 11;
+    int ostanekPriDeljenju = sestevekZmnozkov % 11;
 
     kontrolnaStevilka = 11 - ostanekPriDeljenju;
     if (kontrolnaStevilka == 10) {
@@ -41,7 +44,7 @@ public class PreverjanjeDavcneInEMSO {
 
   public static Boolean preveriEMSO(long emso) {
     long[] tabelaEMSO = new long[13];
-    if(emso > 9999999999999L){
+    if (emso > 9999999999999L) {
       return false;
     }
     for (int i = 12; i >= 0; i--) {
@@ -53,18 +56,19 @@ public class PreverjanjeDavcneInEMSO {
     long sestevekZmnozkov = 0L;
 
 
-    for (int i = 0; i < 12; i++) {
-      sestevekZmnozkov = 7 * tabelaEMSO[0] + 6 * tabelaEMSO[1] + 5 * tabelaEMSO[2] + 4 * tabelaEMSO[3] + 3 * tabelaEMSO[4] + 2 * tabelaEMSO[5] + 7 * tabelaEMSO[6] + 6 * tabelaEMSO[7] + 5 * tabelaEMSO[8] + 4 * tabelaEMSO[9] + 3 * tabelaEMSO[10] + 2 * tabelaEMSO[11];
-    }
+    sestevekZmnozkov = 7 * tabelaEMSO[0] + 6 * tabelaEMSO[1] + 5 * tabelaEMSO[2] + 4 * tabelaEMSO[3] + 3 * tabelaEMSO[4] + 2 * tabelaEMSO[5] + 7 * tabelaEMSO[6] + 6 * tabelaEMSO[7] + 5 * tabelaEMSO[8] + 4 * tabelaEMSO[9] + 3 * tabelaEMSO[10] + 2 * tabelaEMSO[11];
 
     long ostanekPriDeljenju = sestevekZmnozkov % 11;
-    kontrolnaStevilka = 11 - ostanekPriDeljenju;
-    if(kontrolnaStevilka == 11){
-      kontrolnaStevilka=0;
-    }else if(kontrolnaStevilka == 10){
+    if (ostanekPriDeljenju == 0) {
+      kontrolnaStevilka = 0;
+    } else {
+      kontrolnaStevilka = 11 - ostanekPriDeljenju;
+    }
+
+    if (kontrolnaStevilka == 10) {
       return false;
     }
-    
+
     if (kontrolnaStevilka == tabelaEMSO[12]) {
       return true;
     } else {
@@ -72,4 +76,3 @@ public class PreverjanjeDavcneInEMSO {
     }
   }
 }
-
