04dd21ff69bf5f1a2fe5351ecff352f28e6eabdb
uros <uros@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2011-01-29 20:54:27 +0000

    dodatne nastavitve
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@6160 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/db/connection/DbConnection.java
+++ src/com/openitech/db/connection/DbConnection.java
@@ -37,6 +37,7 @@ public interface DbConnection {
   String DB_POOL_EXECUTE_ON_CREATE = "db.pool.execute.on.create";
   String DB_USE_RECONNECT="db.reconnect.use";
   String DB_USE_POOL="db.pool.use";
+  String DB_USE_TEMPORARY_POOL="db.pool.temporary.use";
   String DB_CASE_INSENSITIVE = "db.case.insensitive";
   String DB_USE_WEBROWSET = "db.useWebRowSet";
   String DB_USE_SECONDARY_WEBROWSET = "db.useSecondaryWebRowSet";
