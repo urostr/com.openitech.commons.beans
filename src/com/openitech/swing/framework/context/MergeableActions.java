a894296cddcc79e3a0de4379c578439afae2d8b6
uros <uros@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2011-05-05 11:28:04 +0000

    malce lepse dodajanje Actionov v workareo
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@6955 7140e42c-79bd-4eef-9ff7-51c9bd73192f

---
+++ src/com/openitech/swing/framework/context/MergeableActions.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.openitech.swing.framework.context;
+
+import java.util.List;
+import javax.swing.Action;
+
+/**
+ *
+ * @author uros
+ */
+public interface MergeableActions {
+  public List<Action> getMergeableActions();
+}
