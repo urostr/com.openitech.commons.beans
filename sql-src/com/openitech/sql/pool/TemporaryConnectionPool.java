049378a6187c63dbcd8b99746b5fc3fa5ab3464b
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2011-05-23 14:54:35 +0000

    temp pool
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@7099 7140e42c-79bd-4eef-9ff7-51c9bd73192f

---
+++ sql-src/com/openitech/sql/pool/TemporaryConnectionPool.java
@@ -0,0 +1,129 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openitech.sql.pool;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author domenbasic
+ */
+public class TemporaryConnectionPool {
+
+  private final List<TemporaryPooledConnectionProxy> connections = new ArrayList<TemporaryPooledConnectionProxy>();
+  private final DataSource dataSource;
+  protected boolean autoCommit;
+  private final int initialSize;
+  private java.util.List<String> executeOnCreate;
+  private Semaphore listLock = new Semaphore(1);
+  private Timer cleanup;
+
+  public TemporaryConnectionPool(DataSource dataSource, boolean autoCommit, java.util.List<String> executeOnCreate) {
+    this(dataSource, autoCommit, 0, executeOnCreate);
+  }
+
+  public TemporaryConnectionPool(DataSource dataSource, boolean autoCommit, final int initialSize, java.util.List<String> executeOnCreate) {
+    this.dataSource = dataSource;
+    this.autoCommit = autoCommit;
+    this.executeOnCreate = executeOnCreate;
+    this.initialSize = initialSize;
+
+    for (int i = 0; i < initialSize; i++) {
+      try {
+        connections.add(new TemporaryPooledConnectionProxy(dataSource, autoCommit, executeOnCreate));
+      } catch (SQLException ex) {
+        Logger.getLogger(TemporaryConnectionPool.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+
+    this.cleanup = new Timer("Temp Connection pool cleanup", true);
+    this.cleanup.scheduleAtFixedRate(new TimerTask() {
+
+      @Override
+      public void run() {
+        while (connections.size() > initialSize) {
+          for (int i = 0; i < connections.size(); i++) {
+            try {
+              TemporaryPooledConnectionProxy connection = connections.get(i);
+              if (connection.isClosed()) {
+                removeConnection(connection);
+                break;
+              }
+            } catch (SQLException ex) {
+              Logger.getLogger(TemporaryConnectionPool.class.getName()).log(Level.SEVERE, null, ex);
+            }
+          }
+        }
+      }
+    }, 180000, 10000);
+  }
+
+  private void removeConnection(TemporaryPooledConnectionProxy connection) throws SQLException {
+    try {
+      listLock.acquire();
+      try {
+        connections.remove(connection);
+      } finally {
+        listLock.release();
+      }
+      connection.forceClose();
+    } catch (InterruptedException ex) {
+      Logger.getLogger(TemporaryConnectionPool.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  /**
+   * Get the value of autoCommit
+   *
+   * @return the value of autoCommit
+   */
+  public boolean isAutoCommit() {
+    return autoCommit;
+  }
+
+  /**
+   * Set the value of autoCommit
+   *
+   * @param autoCommit new value of autoCommit
+   */
+  public void setAutoCommit(boolean autoCommit) {
+    this.autoCommit = autoCommit;
+  }
+
+  public synchronized Connection getConnection() throws SQLException {
+    TemporaryPooledConnectionProxy result = null;
+    try {
+      listLock.acquire();
+      try {
+        for (TemporaryPooledConnectionProxy temporaryPooledConnectionProxy : connections) {
+          if (temporaryPooledConnectionProxy.isClosed()) {
+            result = temporaryPooledConnectionProxy;
+            break;
+          }
+        }
+      } finally {
+        listLock.release();
+      }
+    } catch (InterruptedException ex) {
+      Logger.getLogger(TemporaryConnectionPool.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+    if (result == null) {
+      connections.add(result = new TemporaryPooledConnectionProxy(dataSource, autoCommit, executeOnCreate));
+    }
+
+    result.open();
+    return result;
+  }
+}
---
+++ sql-src/com/openitech/sql/pool/TemporaryPooledConnectionProxy.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openitech.sql.pool;
+
+import com.openitech.jdbc.proxy.ConnectionProxy;
+import com.openitech.util.Equals;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.sql.Statement;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author domenbasic
+ */
+public class TemporaryPooledConnectionProxy extends ConnectionProxy {
+
+  private final java.util.Map<String, Object> defaults = new java.util.HashMap<String, Object>();
+
+  public TemporaryPooledConnectionProxy(DataSource dataSource) throws SQLException {
+    this(dataSource, true, null);
+  }
+
+  public TemporaryPooledConnectionProxy(DataSource dataSource, boolean autoCommit, java.util.List<String> executeOnCreate) throws SQLException {
+    super(dataSource, autoCommit, executeOnCreate);
+
+    this.defaults.put("autocommit", this.connection.getAutoCommit());
+    this.defaults.put("readOnly", this.connection.isReadOnly());
+    this.defaults.put("catalog", this.connection.getCatalog());
+    this.defaults.put("transactionIsolation", this.connection.getTransactionIsolation());
+
+    //da vem, da ni v uporabi
+    closed = Boolean.TRUE;
+  }
+
+  private boolean isDefault(String property, Object value) {
+    return Equals.equals(defaults.get(property), value);
+  }
+
+  @Override
+  protected boolean isConnectionActive() {
+    return (System.currentTimeMillis() - getTimestamp() < 30000) || super.isConnectionActive();
+  }
+
+  @Override
+  protected Connection getActiveConnection() throws SQLException {
+    lock();
+    try {
+      if (closed) {
+        open();
+      }
+      return super.getActiveConnection();
+    } finally {
+      unlock();
+    }
+  }
+
+  protected void open() throws SQLException {
+    lock();
+    try {
+      closed = Boolean.FALSE;
+    } finally {
+      unlock();
+    }
+  }
+
+  @Override
+  public void close() throws SQLException {
+    lock();
+    try {
+      if (!closed) {
+        if (!isDefault("autoCommit", getAutoCommit())
+                || !isDefault("readOnly", isReadOnly())
+                || !isDefault("catalog", getCatalog())
+                || !isDefault("transactionIsolation", getTransactionIsolation())) {
+          super.close();
+        } else {
+          for (Statement statement : activeStatemens) {
+            statement.close();
+          }
+          for (Savepoint savepoint : activeSavepoints) {
+            connection.releaseSavepoint(savepoint);
+          }
+          activeStatemens.clear();
+          activeSavepoints.clear();
+          closed = Boolean.TRUE;
+
+
+          Logger.getLogger(TemporaryPooledConnectionProxy.class.getName()).info("PooledConnection released.");
+
+        }
+      }
+
+    } finally {
+      unlock();
+    }
+  }
+
+  public void forceClose() throws SQLException {
+    lock();
+    try {
+      super.close();
+    } finally {
+      unlock();
+    }
+  }
+}
