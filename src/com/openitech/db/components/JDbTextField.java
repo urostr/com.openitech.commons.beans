dbd5ac2d97c638d5511c588e6f019a02657beb35
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2011-10-24 08:32:56 +0000

    mozno je izklucit selectAll ob focusu
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@7809 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/db/components/JDbTextField.java
+++ src/com/openitech/db/components/JDbTextField.java
@@ -81,7 +81,28 @@ public class JDbTextField extends JTextField implements DocumentListener, ListDa
   }
 
   public void this_focusGained(FocusEvent e) {
-    EventQueue.invokeLater(selector);
+    if (!ignoreSelectAll) {
+      EventQueue.invokeLater(selector);
+    }
+  }
+  private boolean ignoreSelectAll = false;
+
+  /**
+   * Get the value of ignoreSelectAll
+   *
+   * @return the value of ignoreSelectAll
+   */
+  public boolean isIgnoreSelectAll() {
+    return ignoreSelectAll;
+  }
+
+  /**
+   * Set the value of ignoreSelectAll
+   *
+   * @param ignoreSelectAll new value of ignoreSelectAll
+   */
+  public void setIgnoreSelectAll(boolean ignoreSelectAll) {
+    this.ignoreSelectAll = ignoreSelectAll;
   }
 
   public void this_focusLost(FocusEvent e) {
