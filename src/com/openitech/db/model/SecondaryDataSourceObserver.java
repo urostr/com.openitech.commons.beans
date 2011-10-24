9c161505468e5368197294481f98f53d9c362d47
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2011-10-24 08:38:59 +0000

    SecondaryDataSourceObserver ... ugotovil da je neuporaben zaenkrat
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@7815 7140e42c-79bd-4eef-9ff7-51c9bd73192f

---
+++ src/com/openitech/db/model/SecondaryDataSourceObserver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.openitech.db.model;
+
+/**
+ *
+ * @author uros
+ */
+public interface SecondaryDataSourceObserver extends DataSourceObserver{
+
+  public DbDataSource getSecondaryDataSource();
+
+  public void setSecondaryDataSource(DbDataSource dataSource);
+
+}
