44685bdb8cf55208e1672e33a5d531838f8d2bab
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2011-10-26 07:53:55 +0000

    dodan filter med workareami, ter autoinsert med workareami
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@7856 7140e42c-79bd-4eef-9ff7-51c9bd73192f

---
+++ src/com/openitech/db/filters/JWorkAreaFilter.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openitech.db.filters;
+
+import com.openitech.db.events.ActiveRowChangeEvent;
+import com.openitech.db.events.ActiveRowChangeListener;
+import com.openitech.db.filters.DataSourceFilters.AbstractSeekType;
+import com.openitech.db.model.DbDataSource;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author domenbasic
+ */
+public class JWorkAreaFilter implements ActiveRowChangeListener {
+
+  private int workAreaId;
+  private DbDataSource dsWorkArea;
+  private DataSourceFilters dataSourceFilters;
+  private DataSourceFilters.AbstractSeekType seekType;
+  private final String otherColumName;
+
+  public JWorkAreaFilter(int workAreaId, DataSourceFilters dataSourceFilters, AbstractSeekType seekType, String otherColumName) {
+    this.workAreaId = workAreaId;
+    this.dataSourceFilters = dataSourceFilters;
+    this.seekType = seekType;
+    this.otherColumName = otherColumName;
+  }
+
+  public int getWorkAreaId() {
+    return workAreaId;
+  }
+
+  public void setWorkAreaDataSource(DbDataSource dataSource) {
+    this.dsWorkArea = dataSource;
+
+    this.dsWorkArea.addActiveRowChangeListener(this);
+  }
+
+  @Override
+  public void activeRowChanged(ActiveRowChangeEvent event) {
+    try {
+      if (seekType instanceof DataSourceFilters.IntegerSeekType) {
+        dataSourceFilters.setSeekValue(seekType, dsWorkArea.getInt(otherColumName));
+      } else if (seekType instanceof DataSourceFilters.SeekType) {
+        dataSourceFilters.setSeekValue(seekType, dsWorkArea.getString(otherColumName));
+      } else {
+        dataSourceFilters.setSeekValue(seekType, dsWorkArea.getString(otherColumName));
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(JWorkAreaFilter.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  @Override
+  public void fieldValueChanged(ActiveRowChangeEvent event) {
+  }
+}
