5ac09e0d3ecbf13c28a2fcd990e1f21b5e63e337
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2011-12-15 11:43:04 +0000

    reqired, ignoreIfEmpty
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@8213 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/value/FieldJoin.java
+++ src/com/openitech/value/FieldJoin.java
@@ -14,10 +14,19 @@ public class FieldJoin {
 
   private Field field;
   private Field otherField;
+  private boolean required;
+  private boolean ignoreIfEmpty;
 
   public FieldJoin(Field field, Field otherField) {
     this.field = field;
     this.otherField = otherField;
+  }
+
+  public FieldJoin(Field field, Field otherField, boolean required, boolean ignoreIfEmpty) {
+    this.field = field;
+    this.otherField = otherField;
+    this.required = required;
+    this.ignoreIfEmpty = ignoreIfEmpty;
   }
 
   public Field getField() {
@@ -26,5 +35,21 @@ public class FieldJoin {
 
   public Field getOtherField() {
     return otherField;
+  }
+
+  public boolean isIgnoreIfEmpty() {
+    return ignoreIfEmpty;
+  }
+
+  public boolean isRequired() {
+    return required;
+  }
+
+  public void setIgnoreIfEmpty(boolean ignoreIfEmpty) {
+    this.ignoreIfEmpty = ignoreIfEmpty;
+  }
+
+  public void setRequired(boolean required) {
+    this.required = required;
   }
 }
