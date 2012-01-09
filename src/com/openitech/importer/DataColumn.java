e1e962f31114747253a433535e0162dc39dbbbce
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2012-01-09 13:01:57 +0000

    uporabimo skupni class
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@8415 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/importer/DataColumn.java
+++ src/com/openitech/importer/DataColumn.java
@@ -12,6 +12,8 @@ import com.openitech.value.fields.Field;
 import com.openitech.value.fields.FieldValue;
 import com.openitech.value.fields.ValueType;
 import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -29,6 +31,8 @@ public class DataColumn {
   private Object value;
   private boolean nullable = true;
   private boolean optional = false;
+  private boolean wasNull = false;
+  private Map<java.lang.Class, Object> values = new HashMap<Class, Object>();
 
   protected DataColumn(java.lang.Class type) {
     this.type = type;
@@ -46,61 +50,95 @@ public class DataColumn {
   }
 
   public void setValue(double value) {
+    this.values.clear();
     if (type.equals(java.lang.Double.class)) {
       this.value = java.lang.Double.valueOf(value);
+      this.values.put(java.lang.Double.class, this.value);
     } else {
       this.value = java.lang.Double.toString(value);
+      this.values.put(java.lang.String.class, this.value);
     }
+  }
+
+  public DataColumn(String value) throws ParseException {
+    this(java.lang.String.class);
+    setValue(value);
   }
 
   public DataColumn(Field field) {
     this(ValueType.getType(field.getType()).getSqlClass());
-    
+
     this.field = field;
-  }  
- 
-  public void setValue(String value) throws ParseException {
+  }
+
+  public boolean wasNull() {
+    return wasNull;
+  }
+
+  public final void setValue(String value) throws ParseException {
+    DataColumn parsedValue = parseValue(value, nullable, type);
+
+    this.values.clear();
+    this.value = parsedValue.value;
+    this.wasNull = parsedValue.wasNull;
+    this.values.putAll(parsedValue.values);
+  }
+
+  public static DataColumn parseValue(String value, boolean nullable, Class type) throws ParseException {
+    DataColumn result = new DataColumn(type, nullable);
+
     if (type.equals(java.lang.Double.class)) {
       if ((value.length() == 0) || value.equalsIgnoreCase("NaN") || value.equalsIgnoreCase("(null)")) {
-        this.value = nullable ? null : 0;
+        result.value = nullable ? null : 0;
+        result.wasNull = true;
       } else {
-        this.value = java.lang.Double.valueOf(nf.parse(value, new java.text.ParsePosition(0)).doubleValue());
+        result.value = java.lang.Double.valueOf(nf.parse(value, new java.text.ParsePosition(0)).doubleValue());
       }
     } else if (type.equals(java.math.BigInteger.class)) {
       if ((value.length() == 0) || value.equalsIgnoreCase("NaN") || value.equalsIgnoreCase("(null)")) {
-        this.value = nullable ? null : 0;
+        result.value = nullable ? null : 0;
+        result.wasNull = true;
       } else {
-        this.value = new java.math.BigInteger(value);
+        result.value = new java.math.BigInteger(value);
       }
     } else if (type.equals(java.lang.Integer.class)) {
       if ((value.length() == 0) || value.equalsIgnoreCase("NaN") || value.equalsIgnoreCase("(null)")) {
-        this.value = nullable ? null : 0;
+        result.value = nullable ? null : 0;
+        result.wasNull = true;
       } else {
-        this.value = java.lang.Integer.valueOf(value);
+        result.value = java.lang.Integer.valueOf(value);
       }
     } else if (type.equals(java.util.Date.class) || type.equals(java.sql.Date.class)) {
       if ((value.length() == 0) || value.equalsIgnoreCase("NaN") || value.equalsIgnoreCase("(null)")) {
-        this.value = null;
+        result.value = null;
       } else if (value.contains(".")) {
-        this.value = new java.sql.Date(df.parse(value).getTime());
+        result.value = new java.sql.Date(df.parse(value).getTime());
       } else if (value.contains("-")) {
-        this.value = new java.sql.Date(dfsql.parse(value).getTime());
+        result.value = new java.sql.Date(dfsql.parse(value).getTime());
       } else {
-        this.value = new java.sql.Date(dfplain.parse(value).getTime());
+        result.value = new java.sql.Date(dfplain.parse(value).getTime());
       }
     } else if (type.equals(java.lang.String.class)) {
       if (value.equalsIgnoreCase("(null)")) {
-        this.value = nullable ? null : "";
+        result.value = nullable ? null : "";
+        result.wasNull = true;
       } else {
-        this.value = value;
+        result.value = value;
       }
     } else if (type.isInstance(value)) {
-      this.value = value;
+      result.value = value;
     } else {
       throw new IllegalArgumentException();
     }
-  }
 
+    if (value == null) {
+      result.wasNull = true;
+    }
+
+    result.values.put(result.type, result.value);
+
+    return result;
+  }
   private Field field = null;
 
   /**
@@ -120,12 +158,13 @@ public class DataColumn {
   public void setField(Field field) {
     this.field = field;
   }
-  
+
   public FieldValue getFieldValue() {
     if (field != null) {
       return new FieldValue(field, getValue());
-    } else
-      throw new IllegalStateException("Field is not set");    
+    } else {
+      throw new IllegalStateException("Field is not set");
+    }
   }
 
   public void reset() {
@@ -134,6 +173,18 @@ public class DataColumn {
 
   public Object getValue() {
     return value;
+  }
+
+  public Object getValue(Class type) throws ParseException {
+    if (values.containsKey(type)) {
+      return values.get(type);
+    } else {
+      DataColumn parseValue = parseValue(this.wasNull || this.value == null ? null : this.value.toString(), nullable, type);
+
+      values.put(type, parseValue.value);
+
+      return parseValue.value;
+    }
   }
 
   public boolean isOptional() {
