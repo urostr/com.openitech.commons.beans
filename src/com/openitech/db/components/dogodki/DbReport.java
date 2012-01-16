542e97398567ddce96acd914d927e10d9adf8831
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2012-01-16 09:16:04 +0000

    report
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@8477 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/db/components/dogodki/DbReport.java
+++ src/com/openitech/db/components/dogodki/DbReport.java
@@ -4,8 +4,10 @@
  */
 package com.openitech.db.components.dogodki;
 
+import com.openitech.db.filters.DataSourceFiltersMap;
 import com.openitech.db.model.DbDataModel;
 import com.openitech.db.model.DbDataSource;
+import com.openitech.db.model.factory.DataSourceConfig;
 import com.openitech.db.model.factory.DataSourceFactory;
 import com.openitech.db.model.factory.JaxbUnmarshaller;
 import com.openitech.db.model.xml.config.Workarea;
@@ -19,7 +21,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.text.Document;
+import javax.xml.bind.JAXBException;
 import net.sf.jasperreports.engine.JasperReport;
 
 /**
@@ -35,6 +40,7 @@ public class DbReport {
   protected String xmlParameters;
   protected Workarea workArea;
   protected SQLWorker sqlWorker = new SQLWorker();
+  private DataSourceFactory factory;
 
   public DbReport(String name, byte[] fileBytes, Blob serializedBlob, String xmlParameters) throws Exception {
     this.name = name;
@@ -45,14 +51,19 @@ public class DbReport {
     ObjectInputStream oin = new ObjectInputStream((serializedBlob).getBinaryStream());
     jasperReport = (JasperReport) oin.readObject();
 
-    new DataSourceFactory(new DbDataModel() {
 
-      @Override
-      public Map<String, Document> getDocuments() {
-        return new HashMap<String, Document>();
-      }
-    }).configure("Report", xmlParameters);
     workArea = (Workarea) JaxbUnmarshaller.getInstance().unmarshall(Workarea.class, xmlParameters);
+    if(workArea != null){
+     final DbDataModel dbDataModel = new DbDataModel() {
+
+        @Override
+        public Map<String, Document> getDocuments() {
+          return new HashMap<String, Document>();
+        }
+      };
+    factory = new DataSourceFactory(dbDataModel);
+    factory.configure("Report", workArea, new DataSourceConfig(dbDataModel));
+    }
   }
 
   public JasperReport getJasperReport() {
@@ -65,21 +76,40 @@ public class DbReport {
 
   public ResultSet getResultSet(DbDataSource sourceDataSource) throws SQLException {
     ResultSet result = null;
+    if (workArea != null) {
+      DataSource waDataSource = workArea.getDataSource();
+      if (waDataSource != null) {
+        String sql = waDataSource.getSQL();
+        if (sql != null) {
+          List parameters = new ArrayList();
+          List<String> eventColumns = waDataSource.getEventColumns();
+          for (String eventCoulmn : eventColumns) {
+            parameters.add(sourceDataSource.getObject(eventCoulmn));
+          }
 
-    DataSource waDataSource = workArea.getDataSource();
-    if (waDataSource != null) {
-      String sql = waDataSource.getSQL();
-      if (sql != null) {
-        List parameters = new ArrayList();
-        List<String> eventColumns = waDataSource.getEventColumns();
-        for (String eventCoulmn : eventColumns) {
-          parameters.add(sourceDataSource.getObject(eventCoulmn));
+          result = sqlWorker.executeQuery(sql, parameters);
         }
-
-        result = sqlWorker.executeQuery(sql, parameters);
       }
+    }
+    if (result == null) {
+      sourceDataSource.reload(false);
+      result = sourceDataSource.getResultSet();
     }
 
     return result;
+  }
+
+  public boolean hasParameters() {
+    if(factory != null){
+      DataSourceFiltersMap filtersMap = factory.getFiltersMap();
+      if(filtersMap != null){
+        return filtersMap.size() > 0;
+      }
+    }
+    return false;
+  }
+
+  public DataSourceFactory getFactory() {
+    return factory;
   }
 }
