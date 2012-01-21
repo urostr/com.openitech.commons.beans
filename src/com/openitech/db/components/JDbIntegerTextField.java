f3d6818a1792a2415b8d1f0017ebe9cc8aefb388
domen <domen@7140e42c-79bd-4eef-9ff7-51c9bd73192f>
2012-01-21 22:14:01 +0000

    commitanje vrednosti v integer fieldu pred shranjevanjem
    git-svn-id: svn://192.168.94.18/inisa/Razvoj/Head/db commons@8554 7140e42c-79bd-4eef-9ff7-51c9bd73192f

--- src/com/openitech/db/components/JDbIntegerTextField.java
+++ src/com/openitech/db/components/JDbIntegerTextField.java
@@ -6,62 +6,107 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package com.openitech.db.components;
 
+import com.openitech.db.model.DbDataSource;
 import com.openitech.text.FormatFactory;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author uros
  */
 public class JDbIntegerTextField extends JDbFormattedTextField {
-  private boolean autosize=true;
-  private int length=0;
-  
-  /** Creates a new instance of JDbIntegerTextField */
+
+  private boolean autosize = true;
+  private int length = 0;
+
+  /**
+   * Creates a new instance of JDbIntegerTextField
+   */
   public JDbIntegerTextField() {
     super();
     setFormat(FormatFactory.getIntegerNumberFormat(0));
   }
-  
-  /** Creates a new instance of JDbIntegerTextField */
+
+  /**
+   * Creates a new instance of JDbIntegerTextField
+   */
   public JDbIntegerTextField(boolean autosize) {
     super();
-    this.autosize=autosize;
+    this.autosize = autosize;
     updateFormat();
   }
 
-  /** Creates a new instance of JDbIntegerTextField */
+  /**
+   * Creates a new instance of JDbIntegerTextField
+   */
   public JDbIntegerTextField(int length, boolean autosize) {
     super();
-    this.autosize=autosize;
-    this.length=length;
+    this.autosize = autosize;
+    this.length = length;
     updateFormat();
   }
 
   public void setAutosize(boolean autosize) {
     this.autosize = autosize;
   }
-  
+
   public boolean isAutosize() {
     return autosize;
   }
 
   protected void updateFormat() {
     setFormat(FormatFactory.getIntegerNumberFormat(length));
-    if (isAutosize())
-      setColumns(length+1);
+    if (isAutosize()) {
+      setColumns(length + 1);
+    }
   }
 
   public void setLength(int length) {
-    this.length=length;
+    this.length = length;
     updateFormat();
   }
 
   public int getLength() {
     return length;
   }
-  
+  public DbDataSource dataSourceForUpdates;
+
+  /**
+   * Get the value of dataSourceForUpdates
+   *
+   * @return the value of dataSourceForUpdates
+   */
+  public DbDataSource getDataSourceForUpdates() {
+    return dataSourceForUpdates;
+  }
+
+  /**
+   * Set the value of dataSourceForUpdates
+   *
+   * @param dataSourceForUpdates new value of dataSourceForUpdates
+   */
+  public void setDataSourceForUpdates(DbDataSource dataSourceForUpdates) {
+    this.dataSourceForUpdates = dataSourceForUpdates;
+    if (this.dataSourceForUpdates != null) {
+      this.dataSourceForUpdates.addActionListener(new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          try {
+            dataSource_actionPerformed(e);
+          } catch (ParseException ex) {
+            Logger.getLogger(JDbIntegerTextField.class.getName()).log(Level.SEVERE, null, ex);
+            throw new IllegalArgumentException(ex);
+          }
+        }
+      });
+    }
+  }
 }
